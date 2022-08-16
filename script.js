0a3303f218744754f9a7269b0e3f5d84d6cdd369
TungAstreek <tung@astreek.com>
2022-08-16 22:59:57 +0700

    First commit

---
+++ script.js
@@ -0,0 +1,11 @@
+(function () {
+  let signupButton = document.querySelector("#signup-button");
+  signupButton.addEventListener("click", validateInput);
+
+  let passwordInput = document.querySelector("#password-inp");
+  let confirmPasswordInput = document.querySelector("#confirm-password-inp");
+
+  function validateInput() {
+
+  }
+})();
